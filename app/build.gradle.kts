a9effc24c68ec24b8cfc76698ed257818ac510c2
malken21 <77374813+malken21@users.noreply.github.com>
2026-04-22 11:43:30 +0900

    chore: bump version to 1.2.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.marumasa.marumaime"
         minSdk = 24
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.2.7"
+        versionCode = 12
+        versionName = "1.2.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
