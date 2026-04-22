46a8cd0a10f8559bac98dc33082c2fd16fd8cbab
malken21 <77374813+malken21@users.noreply.github.com>
2026-04-22 12:59:10 +0900

    chore: bump version to 1.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.marumasa.marumaime"
         minSdk = 24
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.3.0"
+        versionCode = 15
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
