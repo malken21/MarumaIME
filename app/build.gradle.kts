f452d1f4158e7ae30b6c35020426e3612a3ff9c2
malken21 <77374813+malken21@users.noreply.github.com>
2026-04-22 21:14:47 +0900

    chore: bump version to 1.3.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.marumasa.marumaime"
         minSdk = 24
         targetSdk = 35
-        versionCode = 18
-        versionName = "1.3.4"
+        versionCode = 19
+        versionName = "1.3.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
